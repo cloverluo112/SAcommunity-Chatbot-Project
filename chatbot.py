8c74f6b5890651e179651b75f0257b67ed9d2ba5
cloverluo112 <110218255+cloverluo112@users.noreply.github.com>
2024-09-22 23:01:15 +0930

    Update the correct file for chatbot.py
    The chatbot.py was accidentally uploaded in the old version as it was the script right after the team collaboration. Here is the corrected version of it

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,7 @@ import logging
 # Flask
 from flask import Flask, request
 from flask_socketio import SocketIO, emit
-from engineio.async_drivers import eventlet
+# from engineio.async_drivers import eventlet
 
 # Langchain
 from langchain_core.documents import Document
@@ -14,20 +14,16 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import Runnable
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
-
 from langchain.agents import AgentExecutor, create_tool_calling_agent
-
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 # Embedding
 from langchain_community.vectorstores import FAISS
 from langchain_community.retrievers import BM25Retriever
-
 from langchain.retrievers import EnsembleRetriever
 from langchain.pydantic_v1 import BaseModel, Field
 from langchain_community.embeddings import GPT4AllEmbeddings
-
 from langchain_openai import ChatOpenAI
 
 # OpenStreetMap
@@ -35,11 +31,8 @@ from geopy.geocoders import Nominatim
 
 logging.basicConfig(level=logging.DEBUG)
 
-# Environment setup
-# Need to use .env files for security
-os.environ["OPENAI_API_KEY"] = ""
-
-
+# Set up environment variables
+os.environ['OPENAI_API_KEY'] = ''
 
 # Location setup (current)
 council_list = {
@@ -73,9 +66,9 @@ council_list = {
 geolocator = Nominatim(user_agent='chatbot_geocoder')
 
 def get_location(address):
+    """Get the location by using the address."""
     address = address + ', SA, AU'
     location = geolocator.geocode(address, addressdetails=True)
-
     suburb = ''
     council = ''
 
@@ -109,43 +102,36 @@ def get_location(address):
     
     return suburb, council
 
-
-
 # VectorDB setup
 # embedding (GPT4all local embeddings)
 embeddings = GPT4AllEmbeddings()
+
 # FAISS retriever from local database
-faiss=FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+faiss=FAISS.load_local('faiss_index', embeddings, allow_dangerous_deserialization=True)
+
 # BM25 retriever
 docs=[]
 with open('sacommunity.json', 'r') as f:
     doc_list = json.load(f)
 for i in range(len(doc_list)):
     page=Document(page_content = doc_list[i])
-    docs.append(page)   
+    docs.append(page)
+
 bm25_retriever = BM25Retriever.from_documents(docs, k=5)
-
-
 
 # LLM setup (GPT3.5)
 # Trial API keys: limited to 100 API calls per minute
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+llm = ChatOpenAI(model='gpt-3.5-turbo-0125', temperature=0)
 
 # Extract Class tool
 class Extract(BaseModel):
-    """Extract search term to search a database of communities, organisations or services in South Australia.
+    """
+    Extract search term to search a database of communities, organisations or services in South Australia.
     Examples: financial assistance, football club, footy, soccer club, quit smoking, food assistance ...
     """
 
-    ser: str = Field(
-        description="Communities, organisations or services that the customer is interested in, without the location.",
-    )
-    
-    loc: str = Field(None,
-        description="The location of the request if provided.",
-    )
-
-
+    ser: str = Field(description="Communities, organisations or services that the customer is interested in, without the location.",)
+    loc: str = Field(None,description="The location of the request if provided.",)
 
 # Similarity Search function/tool
 @tool (args_schema=Extract) #to use output from Extract tool
@@ -154,22 +140,21 @@ def search_database(ser: str, loc: str = "") -> str:
 
     # If no service, ask user what they want
     if not ser:
-        return "Tell user to input what service they want."
+        return 'Tell user to input what service they want.'
 
     # If no location, perform similarity search on data
     if not loc or loc.lower() == 'sa':
         # FAISS filter not set
         faiss_retriever = faiss.as_retriever(search_kwargs={'k': 10})
-        # ensemble retriever
+        # Ensemble retriever
         ensemble_retriever = EnsembleRetriever(retrievers=[bm25_retriever, faiss_retriever], weights=[0.5, 0.5])
-        
         docs = ensemble_retriever.invoke(ser)
         search_results = []
         for doc in docs:
             search_results.append(doc.page_content)
         if not search_results:
-            return "No relevant organisation found, tell user to rephrase question."
-        return "\n\n".join(search_results)
+            return 'No relevant organisation found, tell user to rephrase question.'
+        return '\n\n'.join(search_results)
         
     # If given location, get the suburb and council from the given location
     sub, coun = get_location(loc)
@@ -179,9 +164,8 @@ def search_database(ser: str, loc: str = "") -> str:
         faiss_retriever = faiss.as_retriever(search_kwargs={'filter': dict(location=sub.lower()), 'k': 10, 'fetch_k': 4000})
         # ensemble retriever
         ensemble_retriever = EnsembleRetriever(retrievers=[bm25_retriever, faiss_retriever], weights=[0.5, 0.5])
-
         docs = ensemble_retriever.invoke(ser)
-        search_results = [f"{loc} suburb is {sub}, council is {coun}."]
+        search_results = [f'{loc} suburb is {sub}, council is {coun}.']
         for doc in docs:
             org = doc.page_content
             if 'S Suburb' not in org:
@@ -192,86 +176,77 @@ def search_database(ser: str, loc: str = "") -> str:
         if len(search_results) <= 20:
             # FAISS filter set to council
             faiss_retriever = faiss.as_retriever(search_kwargs={'filter': dict(council=coun.lower()), 'k': 10, 'fetch_k': 4000})
-
             docs = faiss_retriever.invoke(ser)
             for doc in docs:
                 org = doc.page_content
                 if 'S Suburb' not in org:
                     org += f'\nS Suburb: {sub.title()}\nCouncil: {coun.title()}'
                 search_results.append(org)
-
-        return "\n\n".join(search_results)
+        return '\n\n'.join(search_results)
 
     elif coun:
         # FAISS filter set to council
         faiss_retriever = faiss.as_retriever(search_kwargs={'filter': dict(council=coun.lower()), 'k': 10, 'fetch_k': 4000})
         # ensemble retriever
         ensemble_retriever = EnsembleRetriever(retrievers=[bm25_retriever, faiss_retriever], weights=[0.5, 0.5])
-
         docs = ensemble_retriever.invoke(ser)
-        search_results = [f"{loc} council is {coun}."]
+        search_results = [f'{loc} council is {coun}.']
         for doc in docs:
             org = doc.page_content
             if 'S Suburb' not in org:
                 org += f'\nCouncil: {coun.title()}'
             search_results.append(org)
-        
-        return "\n\n".join(search_results)
+        return '\n\n'.join(search_results)
     
     else:
-        return f"{loc} is not a correct address, tell user to check their location."
-       
-
+        return f'{loc} is not a correct address, tell user to check their location.'
 
 # Agent setup
-system="""You are the friendly customer service assistant for SAcommunity.org, a real website dedicated to helping users find organizations or communities that provide the assistance they need.
-        Step 1, use the tool when needed to extract keyword for similarity search to obtain the relevant organizations. 
-        Step 2, from the result, recommend organisations based on the 'Services' and 'Subjects' they offer, relevant to user query.
-        Step 3, from all relevant organisations, prioritize recommendations based on location, indicated in the 'S Suburb' or 'Council' fields of organisations.
-        Step 4, list out all relevant organizations, strictly using the following format (fill in the relevant "Org Name" and "Org ID"):
-        •⁠ "Org Name" https://sacommunity.org/org/"Org ID"
-        If no relevant organisation found, do not invent or speculate on responses.
-        """
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system",system),
-        MessagesPlaceholder(variable_name='chat_history'),
-        ("user", "{input}"),
-        MessagesPlaceholder(variable_name="agent_scratchpad"),
-    ]
-)
+system="""
+You are the friendly customer service assistant for SAcommunity.org, a real website dedicated to helping users find organizations or communities that provide the assistance they need.
+Step 1, use the tool when needed to extract keyword for similarity search to obtain the relevant organizations. 
+Step 2, from the result, recommend organisations based on the 'Services' and 'Subjects' they offer, relevant to user query.
+Step 3, from all relevant organisations, prioritize recommendations based on location, indicated in the 'S Suburb' or 'Council' fields of organisations.
+Step 4, list out all relevant organizations, strictly using the following format (fill in the relevant "Org Name" and "Org ID"):
+•⁠ "Org Name" https://sacommunity.org/org/"Org ID"
+If no relevant organisation found, do not invent or speculate on responses.
+"""
+prompt = ChatPromptTemplate.from_messages([
+    ('system',system),
+    MessagesPlaceholder(variable_name='chat_history'),
+    ('user', '{input}'),
+    MessagesPlaceholder(variable_name='agent_scratchpad'),
+])
 # Construct the tool calling agent
 agent = create_tool_calling_agent(llm, [search_database], prompt)
 agent_executor: Runnable = AgentExecutor(agent=agent, tools=[search_database], verbose=True)
-
 
 # Chat history setup
 store = {} #to store each clients session id and chat history
 
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    """Retrieve or create a chat message history for a session."""
     if session_id not in store:
         store[session_id] = ChatMessageHistory()
     return store[session_id]
 
+# Create agent with chat history handling
 message_history = ChatMessageHistory()
 agent_with_chat_history = RunnableWithMessageHistory(
     agent_executor,
     get_session_history,
-    input_messages_key="input",
-    history_messages_key="chat_history",
+    input_messages_key='input',
+    history_messages_key='chat_history',
 )
-
-
-
 
 # Connection setup
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet', logger=True)
+socketio = SocketIO(app, cors_allowed_origins='*', async_mode='eventlet', logger=True)
 
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
-     # Generate a unique session ID for the client
+    # Generate a unique session ID for the client
     session_id = str(request.sid)  # Use request.sid from Flask-SocketIO
     print(session_id)
     emit('status', {'message': 'Connected to server'})
@@ -288,12 +263,12 @@ def handle_user_query(data):
         session_id = str(request.sid) #Again, request.sid is used to retrieve the session ID of the client who sent the message.
         user_input = data['userInput']
         result = agent_with_chat_history.invoke(
-            {"input": user_input},
-            config={"configurable": {"session_id": session_id}},
+            {'input': user_input},
+            config={'configurable': {'session_id': session_id}},
         )
         emit('chat_response', {'reply': result['output']})
     except Exception as e:
-        logging.error(f"Error handling user query: {str(e)}", exc_info=True)
+        logging.error(f'Error handling user query: {str(e)}', exc_info=True)
         emit('error', {'error': str(e)})
 
 if __name__ == '__main__':
